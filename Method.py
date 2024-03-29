32c0434fa2bfb7dd9ec10cfc456d649d3dfc7654
LetsDoThisMF <165355617+LetsDoThisMF@users.noreply.github.com>
2024-03-29 21:34:44 +0700

    Add files via upload

---
+++ Method.py
@@ -0,0 +1,9 @@
+class Method:
+    def __init__(self, total):
+        self.total = total
+
+    def trims(self):
+        print("Terima kasih telah berbelanja di toko 41 computer")
+
+    def selesai(self, code):
+        print(f"Kode transaksi: {code}")
